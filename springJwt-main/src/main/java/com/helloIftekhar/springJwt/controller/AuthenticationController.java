0884d80c4a659f1432455dfa68688a3ee91b9931
Phumudzo Tshivhase <65679975+Phumudzo98@users.noreply.github.com>
2024-07-20 15:01:25 +0200

    Update AuthenticationController.java

--- springJwt-main/src/main/java/com/helloIftekhar/springJwt/controller/AuthenticationController.java
+++ springJwt-main/src/main/java/com/helloIftekhar/springJwt/controller/AuthenticationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins ="http://localhost:4200/")
+@CrossOrigin(origins ="https://ravishing-youth-production.up.railway.app")
 public class AuthenticationController {
 
 
