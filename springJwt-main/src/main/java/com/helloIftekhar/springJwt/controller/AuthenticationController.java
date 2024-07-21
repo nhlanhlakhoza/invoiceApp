5ed217aca165e48c658963de46ebb3daa3523fcb
MFACTORY\nhlan <nhlanhlakhoza05@gmail.com>
2024-07-21 12:29:28 +0200

    update

--- springJwt-main/src/main/java/com/helloIftekhar/springJwt/controller/AuthenticationController.java
+++ springJwt-main/src/main/java/com/helloIftekhar/springJwt/controller/AuthenticationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200") // Remove trailing slash
+@CrossOrigin(origins = "*")  // Allow all origins
 public class AuthenticationController {
 
 
